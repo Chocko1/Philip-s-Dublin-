a03ecd2f1b49b0934a6f666a14495778941a0270
Chocko1 <157376674+Chocko1@users.noreply.github.com>
2024-04-02 13:56:51 +0100

    Create sendEmail.js
    Setting up emailing

---
+++ sendEmail.js
@@ -0,0 +1,12 @@
+// sendEmail.js
+
+exports.handler = async (event) => {
+  const { firstName, lastName, email, message } = JSON.parse(event.body);
+
+  // Here you'll add the logic to send the email
+  // For now, let's just return a success response
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Email sent successfully' })
+  };
+};
